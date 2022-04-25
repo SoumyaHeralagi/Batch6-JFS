357d759ab21e703c88da2004043133e3d46c0190
SoumyaHeralagi <101546622+SoumyaHeralagi@users.noreply.github.com>
2022-04-25 21:59:09 +0530

    Add files via upload

---
+++ JAVA/Springioc/src/Airtel.java
@@ -0,0 +1,23 @@
+
+
+import com.cg.springpro.Sim;
+
+public class Airtel implements Sim {
+	
+//	public Airtel() {
+//		System.out.println("airtel constructor is calling");
+//	}
+
+	@Override
+	public void calling() {
+		System.out.println("calling using airtel sim");
+		
+	}
+
+	@Override
+	public void data() {
+		System.out.println("browsing internet using airtel sim");
+		
+	}
+
+}
