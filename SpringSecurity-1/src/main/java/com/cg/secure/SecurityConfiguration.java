942a17f2e1debe648328a170bc7b9e9107a6aba7
SoumyaHeralagi <101546622+SoumyaHeralagi@users.noreply.github.com>
2022-04-25 21:45:58 +0530

    Add files via upload

---
+++ SpringSecurity-1/src/main/java/com/cg/secure/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.cg.secure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+		auth.inMemoryAuthentication()
+		.withUser("blah")
+		.password("blah")
+		.roles("USER")
+		.and()
+		.withUser("foo")
+		.password("foo")
+		.roles("ADMIN");
+	}
+
+	private UserDetailsManagerConfigurer<AuthenticationManagerBuilder, InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder>> and() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();	
+	}
+	@Override
+	protected void configure(HttpSecurity http)throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasRole("USER")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
+
+	}
+
+
+
+
+}
